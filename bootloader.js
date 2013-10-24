59c46dde16fbbf2a8e10181abe8813bcba84a5c4
jmptable <jmptable@mit.edu>
2013-10-24 17:36:30 -0400

    Not ignoring minified files.

--- bootloader.js
+++ bootloader.js
@@ -1,6 +1,6 @@
 javascript:(function(){
 	var beta_nes = document.createElement('SCRIPT');
 	beta_nes.type = 'text/javascript';
-	beta_nes.src = 'https://raw.github.com/jmptable/beta_nes/master/beta_nes-min-book.js';
+	beta_nes.src = 'https://raw.github.com/jmptable/beta_nes/master/beta_nes-min.js';
 	document.getElementsByTagName('head')[0].appendChild(beta_nes);
 })();
